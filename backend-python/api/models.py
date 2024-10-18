8f18aa8f6cdfcaa7ba20143b6e9fae541c612e1d
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-10-17 20:52:52 -0700

    Modifying settings Creating models

--- backend-python/api/models.py
+++ backend-python/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Balance(models.Model):
+    id = models.IntegerField(primary_key=True)
+    amount = models.FloatField()
+
+
+class Portfolio(models.Model):
+    symbol = models.CharField(max_length=10, primary_key=True)
+    quantity = models.IntegerField()
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ("buy", "Buy"),
+        ("sell", "Sell"),
+    ]
+    type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
+    symbol = models.CharField(max_length=10)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
