e8c3f4b70588e5f27f1eae9c70ff4c651bb27883
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-10-24 00:21:09 -0700

    Removed start script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the Django backend server in the background
-cd /backend
-python manage.py runserver 0.0.0.0:8000 &
-
-# Start the Vite frontend dev server (this will be the exposed service)
-cd /frontend
-npm run dev -- --host 0.0.0.0
